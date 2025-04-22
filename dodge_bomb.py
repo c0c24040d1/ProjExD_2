f1f5573c1835d42c794079366965e312c50811cb
c0c24040d1 <c0c24040d1@edu.teu.ac.jp>
2025-04-22 14:59:50 +0900

    prc4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -49,7 +49,9 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
-        
+        if kk_rct.colliderect(bb_rct):
+            print("Game Over")
+            return
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
 
