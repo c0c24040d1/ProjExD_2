003dd99ea8413eb68cd11ac57783160106b0874f
c0c24040d1 <c0c24040d1@edu.teu.ac.jp>
2025-04-22 14:55:51 +0900

    prc2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,16 +1,19 @@
 import os
 import sys
 import pygame as pg
+import random
 
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
     pg.K_UP:(0,-5),
     pg.K_DOWN:(0,+5),
-    pg.K_LEFT:(0,-5),
-    pg.K_RIGHT:(0,+5),
+    pg.K_LEFT:(-5,0),
+    pg.K_RIGHT:(+5,0),
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
 
 
 def main():
@@ -20,16 +23,36 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    bb_img = pg.Surface((20,20))
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
+    bb_img.set_colorkey((0,0,0))
+    bb_rct=bb_img.get_rect()
+    bb_rct.centerx=random.randint(0,WIDTH)
+    bb_rct.centery=random.randint(0,HEIGHT)
+    vx, vy = +5,+5
+
     clock = pg.time.Clock()
     tmr = 0
+
+    def check_bound(rct:pg.Rect) -> tuple[bool,bool]:
+        yoko, tate = True, True
+        if rct.left < 0 or WIDTH  < rct.right:
+            yoko = False
+        if rct.top < 0 or HEIGHT < rct.bottom:
+            tate = False
+        return yoko, tate
+    
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
-
+        
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
+
 
         for key, mv in DELTA.items():
             if key_lst[key]:
@@ -44,7 +67,11 @@ def main():
         # if key_lst[pg.K_RIGHT]:
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx,vy)
+        screen.blit(bb_img,bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
