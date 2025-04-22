b0a13ec977029ee075d784f74c8c6bd610f843e3
c0c24040d1 <c0c24040d1@edu.teu.ac.jp>
2025-04-22 14:58:28 +0900

    prc3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -71,6 +71,11 @@ def main():
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx,vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx*=-1
+        if not tate:
+            vy*=-1
         screen.blit(bb_img,bb_rct)
         pg.display.update()
         tmr += 1
